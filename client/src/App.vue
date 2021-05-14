dc90ac045579ac21023d95b62e30202384f89079
Kim Tae Hoon <eszqsc112@gmail.com>
2021-05-14 19:55:43 +0900

    move: client folder

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,45 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <div>
-    <span>a: {{ a }}</span>
-    <br />
-    <span>b: {{ b }}</span>
-    <br />
-    <span>c: {{ c }}</span>
-    <br />
-    <span>d: {{ d }}</span>
-    <br />
-    <span>e: {{ e }}</span>
-    <br />
-    <pre>{{ env }}</pre>
-    <br />
-  </div>
+  <span>
+    {{ a }}
+  </span>
 </template>
 
 <script lang="ts">
-import { METHODS } from 'http';
 import { defineComponent, ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
   },
-  setup(props) {
-    const a = ref(import.meta.env.NODE_ENV);
-    const b = ref(import.meta.env.DB_HOST);
-    const c = ref(import.meta.env.DB_NAME);
-    const d = ref(import.meta.env.DB_PASSWORD);
-    const e = ref(import.meta.env.VITE_DB_USER);
-    const env = import.meta.env;
-    return {
-      a, b, c, d, e, env
-    };
+  props: {
+    lang: String,
   },
-})
+  setup(props) {
+    return {
+      a: 'hello',
+    }
+  },
+});
 </script>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
+  root: './client',
+  publicDir: './client/public',
   server: {
     port: 29537,
   },
-  
+  define: {},
 });
